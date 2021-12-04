fcb9c1ec2f0e2422e8d21881674ba1a32cf780e7
Andre Blanke <blanke.and@gmail.com>
2021-12-04 19:41:42 +0100

    Improve archive naming

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
+import org.gradle.jvm.tasks.Jar
+
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+import dev.andreblanke.mcmods.modid.build.Mod
 
 plugins {
     kotlin("jvm") version "1.6.0-RC2"
@@ -12,6 +16,12 @@ subprojects {
     tasks {
         withType<KotlinCompile>().all {
             kotlinOptions.jvmTarget = "1.8"
+        }
+
+        withType<Jar>().all {
+            archiveBaseName.set(rootProject.name)
+            archiveAppendix.set(this@subprojects.name)
+            archiveVersion.set(Mod.VERSION)
         }
     }
 }
